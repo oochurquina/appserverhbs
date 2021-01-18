ebbb3887e92c12d40a34b55565eed3bdcd8b482f
OMAR <oochurquina@hotmail.com>
2021-01-18 15:31:51 -0300

    Segundo commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const hbs = require('hbs');
 require('./hbs/helpers');
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname+'/public'));
 // express hbs engine
